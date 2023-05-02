6aa210f5fd60feab098b79dac3dee4862da7053d
Diego Figueroa <diegofigueroa79@gmail.com>
2023-05-02 11:32:46 -0700

    complete roommain script for loading players and loading city scene

--- Assets/Game/Room/RoomMain.cs
+++ Assets/Game/Room/RoomMain.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class RoomMain : MonoBehaviour
+public class RoomMain : MonoBehaviourPunCallbacks
 {
+    [SerializeField]
+    private UIDocument roomUIDocument;
+    private VisualElement root;
+    private VisualElement playerContainer;
+    private List<Label> playerLabelList;
+    private Label roomNameLabel;
+    private Button startButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // initialize ui elements
+        root = roomUIDocument.rootVisualElement;
+        roomNameLabel = root.Q<Label>("RoomNameLabel");
+        startButton = root.Q<Button>("StartButton");
+        playerContainer = root.Q<VisualElement>("PlayerListContainer");
+
+        // add startgame function to startbutton
+        startButton.clicked += () => StartGame();
+
+        // on start of game, set room name label to current room name
+        roomNameLabel.text = $"Room: {PhotonNetwork.CurrentRoom.Name}";
+        UpdatePlayerList();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void UpdatePlayerList(Player newPlayer = null) {
         
+        playerLabelList = playerContainer.Query<Label>().ToList();
+
+        int playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+
+        if (newPlayer != null) {
+            playerLabelList[playerCount - 1].text = newPlayer.NickName;
+            return;
+        }
+
+        for (int i = 0; i < playerCount; i++)
+        {
+            playerLabelList[i].text = PhotonNetwork.PlayerList[i].NickName;    
+        }
     }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        UpdatePlayerList(newPlayer);
+    }
+
+    void StartGame() {
+        if ( PhotonNetwork.IsMasterClient ) {
+            PhotonNetwork.LoadLevel("City");
+        }
+    }
+
 }
