881aa735dfa772ce2edcf131db8a75c2abc71ddc
Diego Figueroa <diegofigueroa79@gmail.com>
2023-05-04 21:54:46 -0700

    add damage and death incomplete functions

--- Assets/Game/City/Player/PlayerMain.cs
+++ Assets/Game/City/Player/PlayerMain.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class PlayerMain : MonoBehaviour
 {
     private PlayerMovement playerMovement;
+    public int health = 100;
 
     private void OnEnable() {
         // when player is instantiated, enable playermovement script
         playerMovement = GetComponent<PlayerMovement>();
         playerMovement.enabled = true;       
+    }
+
+    private void TakeDamage(int damage) {
+        health -= damage;
+        // if health drops below zero, call death function
+        if ( health <= 0 ) {
+            PlayerDeath();
+        }
+    }
+
+    private void PlayerDeath() {
+        // disable movement
+        playerMovement.enabled = false;
+        // disable collider
+        // start coroutine for fadein fadeout black screen?
+        // respawn to new spawn point
     }
 }
