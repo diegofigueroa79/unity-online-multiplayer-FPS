1165e378a53d4ad4b040ff1df176a42f17ba4306
Diego Figueroa <diegofigueroa79@gmail.com>
2023-05-05 19:40:22 -0700

    Add mouse rotation movement

--- Assets/Game/City/Player/PlayerMovement.cs
+++ Assets/Game/City/Player/PlayerMovement.cs
@@ -11,7 +11,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float speed = 3.0f;
     [SerializeField]
-    private float sensitivity = 5.0f;
+    private float sensitivity = 50.0f;
+    private float xRotation;
+    private float x;
+    private float y;
+    private float z;
+    private float mouse_x;
+    private float mouse_y;
+    private float velocity;
 
 
     private void OnEnable() {
@@ -19,28 +26,35 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         if ( view.IsMine ) {
 
             // PLAYER MOVEMENT WITH KEYBOARD
             ////////////////////////////////////////////
-            float x = Input.GetAxisRaw("Horizontal");
-            float z = Input.GetAxisRaw("Vertical");
-            float y = 0.0f;
+            x = Input.GetAxisRaw("Horizontal");
+            z = Input.GetAxisRaw("Vertical");
+            y = 0.0f;
 
             Vector3 input = new Vector3(x: x, y: y, z: z);
             input.Normalize();
 
-            float velocity = speed * Time.deltaTime;
+            velocity = speed * Time.deltaTime;
 
             transform.position += input * velocity;
             
             // PLAYER ROTATION WITH MOUSE
             ////////////////////////////////////////////
-            //float mouse_x = sensitivity * Input.GetAxis("Mouse X");
-            //float mouse_y = sensitivity * Input.GetAxis("Mouse Y");
+            mouse_x = sensitivity * Input.GetAxisRaw("Mouse X");
+            mouse_y = sensitivity * Input.GetAxisRaw("Mouse Y");
 
+            // rotate camera to look up and down
+            xRotation -= mouse_y;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+            // rotate transform to look left and right
+            transform.Rotate(Vector3.up * mouse_x);
         }
     }
 }
